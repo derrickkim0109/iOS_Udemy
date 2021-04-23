07a34579ca36e3af4993a672739289d817858e4f
Derrick Kim <59466342+thlbyl0109@users.noreply.github.com>
2021-04-23 18:04:08 +0900

    21.04.23_Udemy

---
+++ Practice_App/I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Derrick kim on 2021/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
