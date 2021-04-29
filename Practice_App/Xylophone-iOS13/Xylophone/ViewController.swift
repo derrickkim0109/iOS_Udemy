18c0b0f31c2fe5b2b66a4a0a931f7508e16596c8
Derrick Kim <59466342+thlbyl0109@users.noreply.github.com>
2021-04-29 12:14:37 +0900

    21.04.29_Xylophone and Egg Timer

--- Practice_App/Xylophone-iOS13/Xylophone/ViewController.swift
+++ Practice_App/Xylophone-iOS13/Xylophone/ViewController.swift
@@ -6,16 +6,56 @@
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
+// 1. Google -> 2. Stack Overflow -> 3. Implement
+// -> 4. Documentation in Apple -> 5. Customise
+
+
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    // -----------
+    // Properties
+    // -----------
+    
+    var player: AVAudioPlayer!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    // -----------
+    // Actions
+    // -----------
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+       
+        if let buttonTitle = sender.title(for: .normal) {
+           print(buttonTitle)
+            playSound(title: buttonTitle)
+         }
+        //Reduces the sender's (the button that got pressed) opacity to half.
+           sender.alpha = 0.5
+           
+           //Code should execute after 0.2 second delay.
+           DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+               //Bring's sender's opacity back up to fully opaque.
+               sender.alpha = 1.0
+           }
+        
+        
+    }
+    
+    // -----------
+    // Functions
+    // -----------
+    
+    func playSound(title: String) {
+        let url = Bundle.main.url(forResource: title, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
-
--- Practice_App/playground/MyPlayground.playground/Contents.swift
+++ Practice_App/playground/MyPlayground.playground/Contents.swift
@@ -1,21 +1,69 @@
-   let numbers = [45, 73, 195, 533 ]
-    
-    //Write your code here
-    let computedNumbers = [numbers[0] * numbers[1], numbers[1] * numbers[2], numbers[2] * numbers[3], numbers[3] * numbers[0]]
-    
-    
-    print(computedNumbers)
+//   let numbers = [45, 73, 195, 533 ]
+//
+//    //Write your code here
+//    let computedNumbers = [numbers[0] * numbers[1], numbers[1] * numbers[2], numbers[2] * numbers[3], numbers[3] * numbers[0]]
+//
+//
+//    print(computedNumbers)
+//
+//
+//
+//
+//
+//
+//   let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","w","x","y","z"]
+//
+//
+//   let password = alphabet[Int.random(in: 0...25)] + alphabet[Int.random(in: 0...25)] + alphabet[Int.random(in: 0...25)] + alphabet[Int.random(in: 0...25)] +
+//    alphabet[Int.random(in: 0...25)] + alphabet[Int.random(in: 0...25)]
+//
+//   print(password)
+//
+//
+//
+//   var myName = "Derrick"
+//   func greeting(){
+//    print("Hello")
+//
+//    func greeting2(){
+//        print("Hey!!!")
+//    }
+//    greeting2()
+//   }
+//   func greeting3(){
+//
+//   }
+////   for _ in 0...3{
+////    greeting()
+////   }
    
 
    
    
-   
-   
-   let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","w","x","y","z"]
-   
-   
-   let password = alphabet[Int.random(in: 0...25)] + alphabet[Int.random(in: 0...25)] + alphabet[Int.random(in: 0...25)] + alphabet[Int.random(in: 0...25)] +
-    alphabet[Int.random(in: 0...25)] + alphabet[Int.random(in: 0...25)] 
-   
-   print(password)
-   
+   //Don't change this code:
+   func calculator() {
+     let a = Int(readLine()!)! //First input
+     let b = Int(readLine()!)! //Second input
+     
+     add(n1: a, n2: b)
+     subtract(n1: a, n2: b)
+     multiply(n1: a, n2: b)
+     divide(n1: a, n2: b)
+     
+   }
+
+   //Write your code below this line to make the above function calls work.
+   func add(n1:Int, n2: Int){
+       print(n1 + n2)
+   }
+   func subtract(n1: Int, n2: Int){
+       print(n1 - n2)
+   }
+   func multiply(n1:Int, n2: Int){
+       print(n1 * n2)
+   }
+   func divide(n1: Int, n2: Int){
+      print(n1 / n2)
+   }
+
+calculator()
