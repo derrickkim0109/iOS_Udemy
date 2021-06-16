7e8b4e5c3052b5a61c4d19c1df4617c4a9f8fbd9
Derrick Kim <59466342+thlbyl0109@users.noreply.github.com>
2021-06-16 11:38:00 +0900

    21.06.16_ Messenger

--- Practice_App/Messenger/Messenger/Controllers/Login/LoginViewController.swift
+++ Practice_App/Messenger/Messenger/Controllers/Login/LoginViewController.swift
@@ -9,19 +9,134 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    // Properties
+    
+    private let scrollView: UIScrollView = {
+      let scrollView = UIScrollView()
+        scrollView.clipsToBounds = true
+      return scrollView
+    }()
+    
+    private let imageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.image = UIImage(named: "logo")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let emailField: UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .continue
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "Email Address.."
+        
+        // Text 왼쪽 끝에 붙어있는 것 조정
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        field.keyboardType = .emailAddress
+        
+        return field
+    }()
+    
+    private let passwordField: UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .done
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "Password.."
+        
+        // Text 왼쪽 끝에 붙어있는 것 조정
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        
+        // 안보이게
+        field.isSecureTextEntry = true
+        
+        return field
+    }()
+    
+    private let loginButton : UIButton = {
+        let button = UIButton()
+        button.setTitle("Log In", for: .normal)
+        button.backgroundColor = .link
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 12
+        button.layer.masksToBounds = true
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Log In"
-        view.backgroundColor = .white
+        view.backgroundColor = .white 
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register",
                                                             style: .done,
                                                             target: self,
                                                             action: #selector(didTapRegister))
+        // Add subview
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+        scrollView.addSubview(emailField)
+        scrollView.addSubview(passwordField)
+        scrollView.addSubview(loginButton)
+
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        scrollView.frame = view.bounds
+        
+        let size = scrollView.width / 3
+        // Icon
+        imageView.frame = CGRect(x: (scrollView.width - size) / 2,
+                                 y: 150,
+                                 width: size,
+                                 height: size)
+        // Email
+        emailField.frame = CGRect(x: 30,
+                                  y: imageView.bottom + 10,
+                                  width: scrollView.width - 60,
+                                  height: 52)
+        // Password
+        passwordField.frame = CGRect(x: 30,
+                                  y: emailField.bottom + 20,
+                                  width: scrollView.width - 60,
+                                  height: 52)
+        
+        // Button
+        loginButton.frame = CGRect(x: 30,
+                                  y: passwordField.bottom + 20,
+                                  width: scrollView.width - 60,
+                                  height: 52)
+        
+    }
+    
+    @objc private func loginButtonTapped(){
+        guard let email = emailField.text, let password = passwordField.text,
+              !email.isEmpty, !password.isEmpty, password.count >= 6 else {
+            return
+        }
+    }
+    func alertUserLoginError() {
+        <#function body#>
+    }
+    
     @objc private func didTapRegister(){
         let vc = RegisterViewController()
+        vc.title = "Create Account"
+        navigationController?.pushViewController(vc, animated: true)
     }
     
 
---
+++ Practice_App/Messenger/Messenger/Resources/Extensions.swift
@@ -0,0 +1,39 @@
+//
+//  Extensions.swift
+//  Messenger
+//
+//  Created by Derrick on 2021/06/16.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    // Properties
+    
+    public var width : CGFloat {
+        return self.frame.size.width
+    }
+    
+    public var height : CGFloat {
+        return self.frame.size.height
+    }
+    
+    public var top : CGFloat {
+        return self.frame.origin.y
+    }
+    
+    public var bottom : CGFloat {
+        return self.frame.size.height + self.frame.origin.y
+    }
+    
+    public var left : CGFloat {
+        return self.frame.origin.x
+    }
+    
+    public var right : CGFloat {
+        return self.frame.size.width + self.frame.origin.x
+    }
+    
+}
